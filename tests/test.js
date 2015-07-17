cb8a827c6c563c4db41bc99cdf98604bd6a77acc
Jose Falcon <falcon@spotify.com>
2015-07-16 23:16:59 -0400

    Add network test

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,19 @@
 var test = require('tape');
 var library = require('../library');
+var request = require('request');
 
 test('zero area', function (t) {
   t.plan(1);
   t.equal(library.area(0), 0);
+});
+
+test('prevent a network call', function (t) {
+  request('http://www.google.com', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      t.fail('successfully made a network call');
+    } else {
+      t.ok('prevented a network call');
+    }
+    t.end();
+  });
 });
