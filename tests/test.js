6e93be6138c42c5252a784988e88b9cbfa5f6eca
Jose Falcon <falcon@spotify.com>
2015-07-20 20:39:33 -0400

    Add test for /project/package/zip

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,7 @@
 var test = require('tape');
 var library = require('../library');
 var request = require('request');
+var fs = require('fs');
 
 test('zero area', function (t) {
   t.plan(1);
@@ -12,8 +13,16 @@ test('prevent a network call', function (t) {
     if (!error && response.statusCode == 200) {
       t.fail('successfully made a network call');
     } else {
-      t.ok('prevented a network call');
+      t.pass('prevented a network call');
     }
     t.end();
   });
 });
+
+test('/project/package.zip exists', function (t) {
+  t.plan(1);
+
+  fs.exists('/project/package.zip', function(exists) {
+    t.ok(exists);
+  });
+})
